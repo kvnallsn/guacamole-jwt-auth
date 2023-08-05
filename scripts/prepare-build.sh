8016b1b5d14366fb1bc83a040eb293f63eceae3c
Kevin Allison <kvnallsn@gmail.com>
2023-08-05 15:13:22 -0500

    7 fix actions pipeline (#9)
    * create new github actions workflows
    
    The dockerfile used to build the guac image now also builds the plugin
    itself rather than copying from the local directory. This should improve
    the CI workflow and allow the docker image to be build simultaneously
    with the matrixed plugin versions

--- scripts/prepare-build.sh
+++ scripts/prepare-build.sh
@@ -7,9 +7,7 @@ fi
 
 echo fixing guacamole version in guac-manifest.json
 sed -ri 's|("guacamoleVersion": ")[0-9]+\.[0-9]+\.[0-9]+|\1'"${GUACAMOLE_VERSION}"'|g' src/main/resources/guac-manifest.json
-
-echo fixing guacamole version in docker file
-sed -ri 's|(FROM guacamole/guacamole:)[0-9]+\.[0-9]+\.[0-9]+|\1'"${GUACAMOLE_VERSION}"'|g' Dockerfile
+cat src/main/resources/guac-manifest.json
 
 echo fixing guacamole-ext version in pom.xml
 sed -ri 's|(<version>)[0-9]+\.[0-9]+\.[0-9]+(</version> <!-- GUACAMOLE_VERSION -->)|\1'"${GUACAMOLE_VERSION}"'\2|g' pom.xml
